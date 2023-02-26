6c0fd06b87ee0314bd39b023089b51047b3a6433
Cloud Dark <clouddark1798@gmail.com>
2023-02-27 01:57:55 +0700

    fix form

--- withoop/form.php
+++ withoop/form.php
@@ -1,14 +1,15 @@
 <form action="process.php" method="post">
     <div>
         <label for="age1">Person A Age of Death:</label>
-        <input type="number" id="age1" name="age1" min="0">
+        <input type="number" id="age1" name="ageA" min="0">
         <label for="year1">Person A Year of Death:</label>
-        <input type="number" id="year1" name="year1" min="0">
+        <input type="number" id="year1" name="yearA" min="0">
     </div>
     <div>
         <label for="age2">Person B Age of Death:</label>
-        <input type="number" id="age2" name="age2" min="0">
+        <input type="number" id="age2" name="ageB" min="0">
         <label for="year2">Person B Year of Death:</label>
-        <input type="number" id="year2" name="year2" min="0">
+        <input type="number" id="year2" name="yearB" min="0">
     </div>
+   <button type="submit">Calculate</button>
 </form>
