f31172d49b6ab15727b3466411515a3e0b60a997
Cloud Dark <clouddark1798@gmail.com>
2023-02-27 01:58:06 +0700

    Update process.php

--- withoop/process.php
+++ withoop/process.php
@@ -1,19 +1,26 @@
 <?php
 
+include_once('./helper.php');
 include_once('./classwithsaga.php');
 // Example usage
-$personA = new Person(10, 12);
-$personB = new Person(13, 17);
+$personAage= post('ageA');
+$personAyear= post('yearA');
+$personBage= post('ageB');
+$personByear= post('yearB');
+$personA = new Person($personAage, $personAyear);
+$personB = new Person($personBage, $personByear);
 
 $witchProblem = new WitchProblem();
 $witchProblem->addPerson($personA);
 $witchProblem->addPerson($personB);
 $witchProblem->calculateKills();
-
-echo "Person A born on Year = " . ($personA->getYearOfDeath() - $personA->getAgeOfDeath());
-echo ", number of people killed on that year is " . $witchProblem->getKillsForPerson($personA). "<br>";
-
-echo "Person B born on Year = " . ($personB->getYearOfDeath() - $personB->getAgeOfDeath());
-echo ", number of people killed on that year is " . $witchProblem->getKillsForPerson($personB). "<br>";
-
-echo "Average number of kills is " . $witchProblem->getAverageKills(). "<br>";
+echo '
+Given: <br>
+Person A: Age of death = '.$personA->getAgeOfDeath().', Year of Death = '.$personA->getYearOfDeath().'<br>
+Person B: Age of death = '.$personB->getAgeOfDeath().', Year of Death = '.$personB->getAgeOfDeath().'<br>
+Answer:<br>
+Person A born on Year = '.$personA->getYearOfDeath().' – '.$personA->getAgeOfDeath().' = '.($personA->getYearOfDeath() - $personA->getAgeOfDeath()).', number of people killed on year '.($personA->getYearOfDeath() - $personA->getAgeOfDeath()).' is '.$witchProblem->getKillsForPerson($personA).'.<br>
+Person B born on Year = '.$personB->getYearOfDeath().' – '.$personB->getAgeOfDeath().' = '.($personB->getYearOfDeath() - $personB->getAgeOfDeath()).', number of people killed on year '.($personB->getYearOfDeath() - $personB->getAgeOfDeath()).' is '.$witchProblem->getKillsForPerson($personB).'.<br>
+<hr>
+So the average is '. $witchProblem->getAverageKills().'<br>
+';
