e07aa5bbb361c5c9cb9e2b86e23f5559e6e16c23
Cloud Dark <clouddark1798@gmail.com>
2023-02-27 02:38:06 +0700

    trying to add ajax jquery

--- withoop/form.php
+++ withoop/form.php
@@ -1,4 +1,4 @@
-<form action="process.php" method="post">
+<form id="my-form">
     <div>
         <label for="age1">Person A Age of Death:</label>
         <input type="number" id="age1" name="ageA" min="0">
@@ -11,5 +11,27 @@
         <label for="year2">Person B Year of Death:</label>
         <input type="number" id="year2" name="yearB" min="0">
     </div>
-   <button type="submit">Calculate</button>
+    <button type="submit"  value="Submit" >Calculate</button>
 </form>
+<div id="contentData"></div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.js" integrity="sha512-nO7wgHUoWPYGCNriyGzcFwPSF+bPDOR+NvtOYy2wMcWkrnCNPKBcFEkU80XIN14UVja0Gdnff9EmydyLlOL7mQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+$(document).ready(function() {
+  $('#my-form').submit(function(event) {
+    event.preventDefault(); // prevent default form submission
+    var formData = $(this).serialize(); // serialize form data
+    $.ajax({
+      type: 'POST',
+      url: 'process.php',
+      data: formData,
+      success: function(response) {
+        // alert(response); // handle success response
+        $('#contentData').html(response);
+      },
+      error: function() {
+        alert('Error! Please try again.'); // handle error
+      }
+    });
+  });
+});
+</script>
